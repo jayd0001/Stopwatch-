3a4179adbf404b42ed22cdf2f19ed329e3472f9b
Jaideep Singh <jay61226@gmail.com>
2023-01-27 01:25:10 +0530

    Removed unused import

--- src/components/StopWatch.js
+++ src/components/StopWatch.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react"
 import { makeStyles } from "@material-ui/core/styles"
-import Button from "@material-ui/core/Button"
 import Typography from "@material-ui/core/Typography"
 import Box from "@material-ui/core/Box"
 import List from "@material-ui/core/List"
